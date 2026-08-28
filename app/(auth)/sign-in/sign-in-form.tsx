6f01b60769b66e706048342e53a001e7fa547dc3
abdikadir macbook air m4 <wlaaldaadaa33@gmail.com>
2026-08-28 18:03:08 +0300

    Refactor sign-in form component to use consistent quotation marks and improve code readability. Set default values for email and password fields.

--- app/(auth)/sign-in/sign-in-form.tsx
+++ app/(auth)/sign-in/sign-in-form.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client"
 
-import { useActionState } from 'react'
+import { useActionState } from "react"
 
-import { Field, fieldProps } from '@/components/forms/field'
-import { FormError } from '@/components/forms/form-error'
-import { SubmitButton } from '@/components/forms/submit-button'
-import { Input } from '@/components/ui/input'
-import { signInAction, type SignInState } from './actions'
+import { Field, fieldProps } from "@/components/forms/field"
+import { FormError } from "@/components/forms/form-error"
+import { SubmitButton } from "@/components/forms/submit-button"
+import { Input } from "@/components/ui/input"
+import { signInAction, type SignInState } from "./actions"
 
 const initialState: SignInState = {}
 
@@ -17,22 +17,34 @@ export function SignInForm() {
     <form action={formAction} className="space-y-4">
       <FormError message={state.error} />
 
-      <Field name="email" label="Email" required error={state.fieldErrors?.email}>
+      <Field
+        name="email"
+        label="Email"
+        required
+        error={state.fieldErrors?.email}
+      >
         <Input
-          {...fieldProps('email', state.fieldErrors?.email)}
+          {...fieldProps("email", state.fieldErrors?.email)}
           type="email"
           autoComplete="username"
           autoFocus
           required
+          value="abdisalam@blueplastic.so"
         />
       </Field>
 
-      <Field name="password" label="Password" required error={state.fieldErrors?.password}>
+      <Field
+        name="password"
+        label="Password"
+        required
+        error={state.fieldErrors?.password}
+      >
         <Input
-          {...fieldProps('password', state.fieldErrors?.password)}
+          {...fieldProps("password", state.fieldErrors?.password)}
           type="password"
           autoComplete="current-password"
           required
+          value="blueplastic1234"
         />
       </Field>
 
